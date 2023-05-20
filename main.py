890ba82aedad9e0c8c615c3e753f52a8b48ed6c7
Vladimir <kobzev.0826@yandex.ru>
2023-05-20 12:42:20 +0300

    #initial commit add files, all works

---
+++ main.py
@@ -0,0 +1,50 @@
+import requests, random
+import tempfile, os
+import vk_api
+from dotenv import load_dotenv
+
+
+def download_image_tmp(url, params={}):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as temp_file:
+        temp_file.write(response.content)
+        temp_file.flush()
+        return temp_file.name
+
+
+def get_comics_info(url, params={}):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    comics_info = response.json()
+    return {
+        "alt": comics_info['alt'],
+        "img": comics_info['img'],
+        "num": comics_info['num']
+    }
+
+
+def get_max_num_xkcd():
+    return get_comics_info('https://xkcd.com/info.0.json')['num']
+
+
+def download_random_comics():
+    rand_num = random.randint(0, get_max_num_xkcd())
+    url = f'https://xkcd.com/{rand_num}/info.0.json'
+    file_path = download_image_tmp(get_comics_info(url)['img'])
+    return file_path
+
+
+def upload_random_xkcd_comics_to_vk(my_token, vk_community_id):
+    comics_name = download_random_comics()
+    print(f"comics download with file path : {comics_name}")
+    vk_api.add_image_to_community(my_token, vk_community_id, comics_name)
+    os.remove(comics_name)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    my_token = os.getenv('VK_TOKEN')
+    vk_community_id = os.getenv('VK_COMMUNITY_ID')
+
+    upload_random_xkcd_comics_to_vk(my_token,vk_community_id)
---
+++ vk_api.py
@@ -0,0 +1,71 @@
+import requests, os
+from dotenv import load_dotenv
+
+
+def get_my_avaliable_groups(token):
+    params = {
+        'access_token': token,
+        'v': 5.131,
+        'extended': 1,
+        'fields': ['name']
+    }
+    response = requests.get('https://api.vk.com/method/groups.get', params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_upload_url(vk_community_id, access_token):
+    upload_url = f'https://api.vk.com/method/photos.getWallUploadServer?group_id={vk_community_id}&access_token={access_token}&v=5.131'
+    upload_response = requests.get(upload_url)
+    upload_response.raise_for_status()
+    return upload_response.json()['response']
+
+
+def upload_photo(photo_path, access_token,vk_community_id):
+    with open(photo_path, 'rb') as file:
+        files = {'photo': file}
+        upload_server = get_upload_url(vk_community_id, access_token)['upload_url']
+        upload_response = requests.post(upload_server, files=files)
+        upload_response.raise_for_status()
+        uploaded_photo = upload_response.json()
+    return uploaded_photo
+
+
+def saveWallPhoto(access_token, vk_community_id, uploaded_photo):
+    # Save uploaded photo to VK
+    save_url = f'https://api.vk.com/method/photos.saveWallPhoto'
+    params = {
+        'access_token': access_token,
+        'v': 5.131,
+        'group_id': vk_community_id,
+        'photo': uploaded_photo["photo"],
+        'server': uploaded_photo["server"],
+        'hash': uploaded_photo["hash"]
+    }
+    save_response = requests.get(save_url, params=params)
+    saved_photo = save_response.json()['response'][0]
+
+    # Post the image to your community
+    post_url = f'https://api.vk.com/method/wall.post'
+    post_data={
+        'access_token' : access_token,
+        'v':5.131,
+        'owner_id' : f"-{vk_community_id}",
+        'attachments' : f'photo{saved_photo["owner_id"]}_{saved_photo["id"]}'
+    }
+    response = requests.post(post_url, data=post_data)
+    response.raise_for_status()
+
+
+def add_image_to_community(access_token, vk_community_id, photo_path):
+    uploaded_photo = upload_photo(photo_path, access_token,vk_community_id)
+    saveWallPhoto(access_token,vk_community_id,uploaded_photo)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    my_token = os.getenv('VK_TOKEN')
+    vk_community_id = os.getenv('VK_COMMUNITY_ID')
+    # my_communities = get_my_avaliable_groups(my_token)
+    # print(my_communities)
+    add_image_to_community(my_token, vk_community_id, 'comics/python.png')
