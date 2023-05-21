173e52b74335a7f0a8c107e510460817072d8ce0
Vladimir <kobzev.0826@yandex.ru>
2023-05-21 14:26:07 +0300

    # change after code review about pipelines

--- main.py
+++ main.py
@@ -28,24 +28,20 @@ def get_max_num_xkcd():
     return get_comics_info('https://xkcd.com/info.0.json')['num']
 
 
-def download_random_comics():
-    rand_num = random.randint(0, get_max_num_xkcd())
-    url = f'https://xkcd.com/{rand_num}/info.0.json'
-    file_path = download_image_tmp(get_comics_info(url)['img'])
-    return file_path
-
-
 def upload_random_xkcd_comics_to_vk(my_token, vk_community_id):
-    comics_name = download_random_comics()
-    vk_api.add_image_to_community(my_token, vk_community_id, comics_name)
-    return comics_name
+    try:
+        right_limit_random = get_max_num_xkcd()
+        rand_num = random.randint(0, right_limit_random)
+        url = f'https://xkcd.com/{rand_num}/info.0.json'
+        image_url = get_comics_info(url)['img']
+        comics_name = download_image_tmp(image_url)
+        vk_api.add_image_to_community(my_token, vk_community_id, comics_name)
+    finally:
+        os.remove(comics_name)
 
 
 if __name__ == '__main__':
     load_dotenv()
     vk_token = os.getenv('VK_TOKEN')
     vk_community_id = os.getenv('VK_COMMUNITY_ID')
-    try:
-        comics_name = upload_random_xkcd_comics_to_vk(vk_token, vk_community_id)
-    finally:
-        os.remove(comics_name)
+    upload_random_xkcd_comics_to_vk(vk_token, vk_community_id)
