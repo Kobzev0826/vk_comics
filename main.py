a70d6010860373a809242ca112a29434d6363553
Vladimir <kobzev.0826@yandex.ru>
2023-05-22 20:27:22 +0300

    # use another way for enviroment

--- main.py
+++ main.py
@@ -29,19 +29,20 @@ def get_max_num_xkcd():
 
 
 def upload_random_xkcd_comics_to_vk(my_token, vk_community_id):
-    try:
-        right_limit_random = get_max_num_xkcd()
-        rand_num = random.randint(0, right_limit_random)
-        url = f'https://xkcd.com/{rand_num}/info.0.json'
-        image_url = get_comics_info(url)['img']
-        comics_name = download_image_tmp(image_url)
-        vk_api.add_image_to_community(my_token, vk_community_id, comics_name)
-    finally:
-        os.remove(comics_name)
+    right_limit_random = get_max_num_xkcd()
+    rand_num = random.randint(0, right_limit_random)
+    url = f'https://xkcd.com/{rand_num}/info.0.json'
+    image_url = get_comics_info(url)['img']
+    comics_name = download_image_tmp(image_url)
+    vk_api.add_image_to_community(my_token, vk_community_id, comics_name)
+    return comics_name
 
 
 if __name__ == '__main__':
     load_dotenv()
-    vk_token = os.getenv('VK_TOKEN')
-    vk_community_id = os.getenv('VK_COMMUNITY_ID')
-    upload_random_xkcd_comics_to_vk(vk_token, vk_community_id)
+    vk_token = os.environ['VK_TOKEN']
+    vk_community_id = os.environ['VK_COMMUNITY_ID']
+    try:
+        comics_name = upload_random_xkcd_comics_to_vk(vk_token, vk_community_id)
+    finally:
+        os.remove(comics_name)
